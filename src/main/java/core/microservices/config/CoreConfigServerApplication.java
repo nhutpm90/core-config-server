e70adba5094964dc2646042d2db92629f01ba456
nhut_pham <nhut.pham@ilogic.vn>
2022-07-29 10:11:51 +0700

    init proj

---
+++ src/main/java/core/microservices/config/CoreConfigServerApplication.java
@@ -0,0 +1,13 @@
+package core.microservices.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/microservices/config/CoreConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package core.microservices.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
